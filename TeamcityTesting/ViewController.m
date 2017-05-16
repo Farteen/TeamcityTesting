3cd134ac825727d36316a019623728b53036e5d4
徐家伟 <xujiawei@autohome.com.cn>
2017-05-16 17:03:56 +0800

    移除Main.storyboard

--- TeamcityTesting/ViewController.m
+++ TeamcityTesting/ViewController.m
@@ -16,6 +16,17 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+    UIAlertController *alertController = [[UIAlertController alloc] init];
+    UIAlertAction *alertAction =  [UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+
+    }];
+    [alertController addAction:alertAction];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    [self presentViewController:alertController animated:YES completion:^{
+        
+    }];
+    });
+
   // Do any additional setup after loading the view, typically from a nib.
 }
 
